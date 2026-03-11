ed6add64fdb9204e2b0870b2efa3a08999fed1ea
Diego Florez <dfflorez60@ucatolica.edu.co>
2026-03-11 11:00:01 -0500

    recover login integration with backend

--- Fronted/src/Login.jsx
+++ Fronted/src/Login.jsx
@@ -1,10 +1,66 @@
-import React from "react";
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
 import { motion } from "framer-motion";
 import "./css/Login.css";
 import ImagenFondo from "./assets/C_C_08.jpg";
 import logoUCatolica from "./assets/LOGO-LOGIN.svg";
 
 const Login = () => {
+
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [user, setUser] = useState(null);
+
+  const navigate = useNavigate();
+
+    const handleSubmit = async (e) => {
+    e.preventDefault();
+
+      try {
+      const response = await fetch("http://127.0.0.1:8000/users/login", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json"
+        },
+        body: JSON.stringify({
+          username,
+          password
+        })
+      });
+
+      const data = await response.json();
+
+      localStorage.setItem("token", data.access_token);
+      getMe();
+      navigate("/");
+      console.log("Respuesta login:", data);
+    } catch (error) {
+      console.error("Error al iniciar sesión:", error);
+    }
+  };
+
+  const getMe = async () => {
+    const token = localStorage.getItem("token");
+
+    try {
+      const response = await fetch("http://127.0.0.1:8000/users/me", {
+        method: "GET",
+        headers: {
+          "Authorization": `Bearer ${token}`
+        }
+      });
+
+      const data = await response.json();
+
+      setUser(data);
+      console.log("Usuario autenticado:", data);
+
+    } catch (error) {
+      console.error("Error consultando /users/me:", error);
+    }
+  };
+
+  
   return (
     <motion.div
       className="login-container"
@@ -29,18 +85,22 @@ const Login = () => {
 
       {/* LADO DERECHO */}
       <div className="login-right">
-        <form className="login-form">
+        <form className="login-form" onSubmit={handleSubmit}>
           <h2>Iniciar Sesión</h2>
 
           <input
             type="text"
             placeholder="Usuario"
+            value={username}
+            onChange={(e) => setUsername(e.target.value)}
             required
           />
 
           <input
             type="password"
             placeholder="Contraseña"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
             required
           />
 
@@ -49,6 +109,12 @@ const Login = () => {
           <p className="extra">
             ¿No tienes cuenta? <span>Regístrate</span>
           </p>
+          {user && (
+            <p className="extra">
+              Bienvenido, <strong>{user.username}</strong>
+          </p>
+          )}
+
         </form>
       </div>
 
